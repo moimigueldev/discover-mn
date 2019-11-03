e2e2756f3775b4345e7afb4f2a4bf37e8005bb5d
Moises Miguel <moimigueldev@gmail.com>
2019-11-03 15:38:28 -0600

    saving unvisited ip address to firebase

--- routes/visitor.js
+++ routes/visitor.js
@@ -2,17 +2,40 @@ const express = require('express'),
     router = express.Router(),
     request = require('request');
 
+// FIREBASE
+const admin = require('firebase-admin');
+const serviceAccount = require('./../ServiceAccountKey');
+
+admin.initializeApp ({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+
 router.get('/', async (req, res) => {
 
-    request.get('https://json.geoiplookup.io/', (err, response, body) => {
-    
-        const ipAdress = JSON.parse(body).ip;
+    db.collection('discover-mn').doc('visitors').get().then(doc => {
+        let ipAddressArr = doc.data().ipAddress;
+        console.log('db', ipAddressArr);
 
-        console.log('res', ipAdress)
+        request.get('https://json.geoiplookup.io/', (err, response, body) => {
+            const visitorIpAddress = JSON.parse(body).ip;
+            ipAddressArr.includes('77.41.012.174')?console.log('Ip Already Exist'):addIpAddress(ipAddressArr, visitorIpAddress);
+    })// end of ipLookup
+    }).catch(err => {
+        console.log('err', err)
     })
-
+  
     res.sendStatus('ok')
-})
+});
+
+const addIpAddress = (ipAddressArr, visitorIpAddress) => {
+    db.collection('discover-mn').doc('visitors').update({
+        ipAddress: [...ipAddressArr, visitorIpAddress]
+    }).then(res => console.log('added new Ip Adress', res))
+    .catch(err => console.log('err adding new ipAddress', err))
+};//end of addIpAdress
 
 
 module.exports = router;
